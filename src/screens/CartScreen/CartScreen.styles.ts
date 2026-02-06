d374f2470d564abbe9c264ce1c0ee25bc977aae4
Shan8 <shanaczanecruzat@gmail.com>
2026-02-06 13:31:16 +0800

    change style

--- src/screens/CartScreen/CartScreen.styles.ts
+++ src/screens/CartScreen/CartScreen.styles.ts
@@ -13,24 +13,25 @@ export const createStyles = (colors: ThemeColors) =>
       flex: 1,
     },
     scrollContent: {
-      padding: 20,
-      paddingBottom: 180,
+      padding: 16,
+      paddingBottom: 20,
     },
     emptyContainer: {
       flex: 1,
       justifyContent: "center",
       alignItems: "center",
-      padding: 40,
+      padding: 32,
     },
     emptyIcon: {
       fontSize: 80,
+      marginBottom: 16,
     },
     emptyTitle: {
       fontSize: 24,
-      fontWeight: "700",
+      fontWeight: "bold",
       color: colors.text,
-      marginTop: 20,
       marginBottom: 8,
+      textAlign: "center",
     },
     emptyText: {
       fontSize: 16,
@@ -42,7 +43,7 @@ export const createStyles = (colors: ThemeColors) =>
       backgroundColor: colors.primary,
       paddingVertical: 14,
       paddingHorizontal: 32,
-      borderRadius: 10,
+      borderRadius: 12,
     },
     shopButtonText: {
       color: colors.buttonText,
@@ -50,22 +51,11 @@ export const createStyles = (colors: ThemeColors) =>
       fontWeight: "600",
     },
     bottomContainer: {
-      position: "absolute",
-      bottom: 0,
-      left: 0,
-      right: 0,
       backgroundColor: colors.card,
-      padding: 20,
       borderTopWidth: 1,
       borderTopColor: colors.border,
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: -2,
-      },
-      shadowOpacity: 0.1,
-      shadowRadius: 4,
-      elevation: 5,
+      paddingVertical: 16,
+      paddingHorizontal: 16,
     },
     summaryContainer: {
       marginBottom: 16,
@@ -73,7 +63,6 @@ export const createStyles = (colors: ThemeColors) =>
     summaryRow: {
       flexDirection: "row",
       justifyContent: "space-between",
-      alignItems: "center",
       marginBottom: 8,
     },
     summaryLabel: {
@@ -82,8 +71,8 @@ export const createStyles = (colors: ThemeColors) =>
     },
     summaryValue: {
       fontSize: 16,
-      fontWeight: "600",
       color: colors.text,
+      fontWeight: "500",
     },
     divider: {
       height: 1,
@@ -93,17 +82,16 @@ export const createStyles = (colors: ThemeColors) =>
     totalRow: {
       flexDirection: "row",
       justifyContent: "space-between",
-      alignItems: "center",
-      marginBottom: 16,
+      marginTop: 8,
     },
     totalLabel: {
-      fontSize: 20,
-      fontWeight: "700",
+      fontSize: 18,
+      fontWeight: "600",
       color: colors.text,
     },
     totalValue: {
-      fontSize: 24,
-      fontWeight: "700",
+      fontSize: 20,
+      fontWeight: "bold",
       color: colors.primary,
     },
     checkoutButton: {
@@ -111,18 +99,18 @@ export const createStyles = (colors: ThemeColors) =>
       paddingVertical: 16,
       borderRadius: 12,
       alignItems: "center",
+      marginBottom: 12,
     },
     checkoutButtonText: {
       color: colors.buttonText,
       fontSize: 18,
-      fontWeight: "700",
+      fontWeight: "bold",
     },
     clearButton: {
       backgroundColor: colors.error,
       paddingVertical: 12,
-      borderRadius: 10,
+      borderRadius: 12,
       alignItems: "center",
-      marginTop: 12,
     },
     clearButtonText: {
       color: colors.buttonText,
